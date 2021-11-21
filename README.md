ff4f7fe739e27ade32d21389744fe6aa247995d1
Hugo Landau <hlandau@devever.net>
2021-11-21 12:31:09 +0000

    Initial commit
    ©! I, Hugo Landau <hlandau@devever.net>, hereby licence these changes under the
    ©! licence with SHA256 hash
    ©! 297b6f030943c45c4c21d1f11c97da3f1a762cbb38719a0f0804d381f70a0931.

---
+++ README.md
@@ -0,0 +1,66 @@
+winsvctool
+==========
+
+This is an open source replacement for `SRVANY.EXE`.
+
+Description of SRVANY
+---------------------
+
+`SRVANY.EXE` was a proprietary tool included in the Windows 2003 Resource Kit
+and no longer maintained by Microsoft. It also came with a utility
+`INSTSRV.EXE` which installs services which use `SRVANY.EXE`.
+
+The `INSTSRV` utility used the following arguments:
+
+    instsrv <service-name> <path-to-SRVANY.EXE> [-a <account-name>] [-p <account-password>]
+    instsrv <service-name> remove
+
+This creates a service using `SRVANY` as the service executable.
+
+Windows stores service configuration information in `HKLM\SYSTEM\CurrentControlSet\Services\<service-name>`. `SRVANY` needs these configuration parameters to be added to its key, beyond the standard Windows ones:
+
+  - `Parameters\Application`: String. Required. The actual service executable to launch and be supervised by `SRVANY`.
+  - `Parameters\AppParameters`: String. Optional. Parameters to pass to the actual service executable.
+  - `Parameters\AppEnvironment`: Multi-String. Optional. Environment variables to pass to the actual service executable.
+  - `Parameters\AppDirectory`: String. Optional. Working directory to run actual service executable under.
+
+Description of winsvctool
+-------------------------
+
+winsvctool is a simple Go daemon which can be hosted as a Windows service and
+which spawns and supervises one other process. It uses
+[hlandau/service](https://github.com/hlandau/service) and inherits its Windows
+service support from there.
+
+    winsvctool -service.*=...
+
+For simplicity, arguments are taken from the service command line rather than
+separate registry keys as in the `SRVANY` case. The following command line
+arguments are supported:
+
+    winsvctool
+      -winsvctool.run=...            Windows commandline to spawn (quote both EXE and arguments).
+      -winsvctool.arg=...            Add an argument to the command to spawn. May be specified multiple times.
+      -winsvctool.cwd=...            Set current working directory to this before spawning (optional).
+
+winsvctool will propagate failures. That is, if the spawned process fails for
+any reason, winsvctool will also fail and exit so that the Windows service
+management system can detect this failure, log it, and restart the entire
+hierarchy, winsvctool and all.
+
+winsvctool generates log output using xlog, so you can logs its (small number
+of) log messages using any method supported by xlog. This is separate from the
+logging of the daemon it supervises.
+
+TODO
+----
+
+- Send the Windows equivalent of SIGINT instead of just terminating the child process.
+  This appears to be hard to do.
+
+Licence
+-------
+
+Licenced under the GPLv3 or later.
+
+© 2021 [Hugo Landau](https://www.devever.net/~hl/)
---
+++ main.go
@@ -0,0 +1,133 @@
+// winsvctool is an adapter utility for running arbitrary daemons as Windows services.
+package main
+
+import (
+	"github.com/hlandau/dexlogconfig"
+	"github.com/hlandau/xlog"
+	"gopkg.in/hlandau/easyconfig.v1"
+	"gopkg.in/hlandau/service.v2"
+	"os"
+	"os/exec"
+)
+
+var log, Log = xlog.New("winsvctool")
+
+// Configuration for the daemon.
+type Config struct {
+	Run string   `usage:"Path to service executable to spawn" default:""`
+	Arg []string `usage:"Argument to pass to service executable (specify multiple times)" default:""`
+	CWD string   `usage:"Working directory to use for spawned service" default:""`
+}
+
+type ctlEventType int
+
+const (
+	ctlTerminated ctlEventType = iota
+	ctlStopReq
+)
+
+type ctlEvent struct {
+	Type     ctlEventType
+	Error    error
+	DoneChan chan error
+}
+
+// Main object for this daemon.
+type Supervisor struct {
+	cfg     Config
+	cmd     *exec.Cmd
+	ctlChan chan ctlEvent
+}
+
+func New(cfg *Config) (*Supervisor, error) {
+	sup := &Supervisor{
+		cfg:     *cfg,
+		ctlChan: make(chan ctlEvent, 2),
+	}
+
+	log.Debugf("supervisor instantiated")
+	return sup, nil
+}
+
+func (sup *Supervisor) Start() error {
+	log.Debugf("starting supervisor...")
+
+	sup.cmd = exec.Command(sup.cfg.Run, sup.cfg.Arg...)
+	sup.cmd.Dir = sup.cfg.CWD
+	err := sup.cmd.Start()
+	if err != nil {
+		log.Criticale(err, "could not start service to be supervised by winsvctool")
+		return err
+	}
+
+	go sup.ctlLoop()
+	go sup.waitTerm()
+
+	return nil
+}
+
+func (sup *Supervisor) ctlLoop() {
+	var pendingStopReq chan error
+	for {
+		ev := <-sup.ctlChan
+		switch ev.Type {
+		case ctlTerminated:
+			if pendingStopReq != nil {
+				pendingStopReq <- ev.Error
+			} else {
+				if ev.Error != nil {
+					log.Criticale(ev.Error, "service supervised by winsvctool exited unexpectedly with error")
+				} else {
+					log.Critical("service supervised by winsvctool exited unexpectedly without error")
+				}
+				// This should not happen, so just exit with error so the Windows service
+				// manager will restart us.
+				os.Exit(3)
+			}
+		case ctlStopReq:
+			if pendingStopReq != nil {
+				panic("unreachable")
+			}
+			pendingStopReq = ev.DoneChan
+
+			err := sup.cmd.Process.Kill()
+			log.Errore(err, "failed to kill supervised process, continuing...")
+		}
+	}
+}
+
+func (sup *Supervisor) waitTerm() {
+	err := sup.cmd.Wait()
+	sup.ctlChan <- ctlEvent{Type: ctlTerminated, Error: err}
+}
+
+func (sup *Supervisor) Stop() error {
+	log.Debugf("processing request to stop supervised process...")
+	doneCh := make(chan error)
+	sup.ctlChan <- ctlEvent{Type: ctlStopReq, DoneChan: doneCh}
+	err := <-doneCh
+	if err != nil {
+		log.Noticee(err, "request to stop supervised process completed with error")
+	} else {
+		log.Notice("request to stop supervised process completed")
+	}
+	return nil
+}
+
+func main() {
+	cfg := &Config{}
+	config := easyconfig.Configurator{
+		ProgramName: "winsvctool",
+	}
+	config.ParseFatal(cfg)
+	dexlogconfig.Init()
+
+	service.Main(&service.Info{
+		Name:          "winsvctool",
+		Description:   "Windows service hosting adapter",
+		DefaultChroot: service.EmptyChrootPath,
+		NewFunc: func() (service.Runnable, error) {
+			return New(cfg)
+		},
+	})
+}
